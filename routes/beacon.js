3b7dfc03512fb43f48c934a9f0a6bd31d72805cd
Andre Vale <andrevale_6@hotmail.com>
2024-07-09 17:17:41 +0100

    add controller positionVBeacons

--- routes/beacon.js
+++ routes/beacon.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const beacon = require('../controllers/beacon');
 
 router.get('/beacons', beacon.getAllBeacons);
+router.get('/beacons/by-size', beacon.getBeaconsByMatrixSize);
+router.get('/matrices/:matrixId/beacons', beacon.getBeaconsForMatrix);
 
 module.exports = router;
